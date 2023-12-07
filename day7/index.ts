4550f752d00064aebeece0b6410e957c0c59b27e
Sébastien Cottinet <scottinet@protonmail.com>
2023-12-07 07:20:58 +0100

    feat: day7, part2

--- day7/index.ts
+++ day7/index.ts
@@ -1,12 +1,14 @@
 import { chrono } from '../utils/chrono';
 import { Hand } from './models/hand.type';
 import { part1 } from './part1';
+import { part2 } from './part2';
 import { parse } from './utils/parse';
 
 async function main(inputFileName: string): Promise<void> {
   const hands = await parse(inputFileName);
 
   chrono<Hand[]>(part1, hands, 'part1');
+  chrono<Hand[]>(part2, hands, 'part2');
 }
 
 main(process.argv[2]);
---
+++ day7/part2.ts
@@ -0,0 +1,64 @@
+import { Hand } from './models/hand.type';
+
+export const CARD_TYPES = 'J23456789TQKA';
+
+function groupCards(cards: string): number[] {
+  let groups: number[] = [];
+
+  for (const card of cards.split('').filter((c) => c !== 'J')) {
+    const index = CARD_TYPES.indexOf(card);
+    groups[index] = groups[index] ? groups[index] + 1 : 1;
+  }
+
+  groups = groups.filter((v) => v > 1).sort((a, b) => b - a);
+
+  if (groups.length === 0) groups = [1];
+
+  const jokers = cards.split('').filter((c) => c === 'J').length;
+
+  groups[0] = Math.min(jokers + groups[0], 5);
+
+  if (groups.reduce((a, b) => a + b, 0) > 5) groups.pop();
+
+  return groups;
+}
+
+function compareGroups(a: Hand, b: Hand): number {
+  const firstGroupDiff = a.groups[0] - b.groups[0];
+  if (firstGroupDiff !== 0) {
+    return firstGroupDiff;
+  }
+
+  const groupsCountDiff = a.groups.length - b.groups.length;
+  if (groupsCountDiff !== 0) {
+    return groupsCountDiff;
+  }
+
+  for (let i = 0; i < a.cards.length; i++) {
+    const aIndex = CARD_TYPES.indexOf(a.cards[i]);
+    const bIndex = CARD_TYPES.indexOf(b.cards[i]);
+
+    if (aIndex !== bIndex) {
+      return aIndex - bIndex;
+    }
+  }
+
+  throw new Error('hands are equal');
+}
+
+export function part2(hands: Hand[]): void {
+  let winnings = 0;
+
+  hands.forEach((hand) => {
+    hand.groups = groupCards(hand.cards);
+  });
+
+  hands.sort(compareGroups);
+
+  for (let i = 0; i < hands.length; i++) {
+    const hand = hands[i];
+    winnings += (i + 1) * hand.bid;
+  }
+
+  console.log(`Winnings: ${winnings}`);
+}
