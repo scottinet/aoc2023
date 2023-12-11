a9f56352747c14bcc8da0846adb3cf630e819646
Sébastien Cottinet <scottinet@protonmail.com>
2023-12-11 12:32:56 +0100

    refactor: expanding universe is for part1 only

--- day11/part1.ts
+++ day11/part1.ts
@@ -1,5 +1,41 @@
 import { Point } from './types/point.type';
 
+function expandUniverse(universe: string[]): string[] {
+  let emptyColumns: Uint8Array = new Uint8Array(universe[0].length).fill(1);
+  let emptyLines: Uint8Array = new Uint8Array(universe.length).fill(1);
+
+  for (let y = 0; y < universe.length; y++) {
+    if (universe[y].indexOf('#') !== -1) {
+      universe[y].split('').forEach((char, x) => {
+        if (char === '#') emptyColumns[x] = 0;
+      });
+    } else {
+      emptyLines[y] = 0;
+    }
+  }
+
+  let extendedUniverse: string[] = [];
+  const newUniverseWidth =
+    universe[0].length + emptyColumns.reduce((acc, curr) => acc + curr, 0);
+
+  for (let y = 0; y < universe.length; y++) {
+    let newLine = '';
+
+    universe[y].split('').forEach((char, x) => {
+      if (emptyColumns[x] === 1) newLine += '.';
+      newLine += char;
+    });
+
+    extendedUniverse.push(newLine);
+
+    if (emptyLines[y] === 0) {
+      extendedUniverse.push('.'.repeat(newUniverseWidth));
+    }
+  }
+
+  return extendedUniverse;
+}
+
 function getGalaxies(universe: string[]): Point[] {
   const galaxies: Point[] = [];
 
@@ -15,6 +51,7 @@ function getGalaxies(universe: string[]): Point[] {
 }
 
 export function part1(universe: string[]): void {
+  universe = expandUniverse(universe);
   const galaxies = getGalaxies(universe);
   let totalDistance = 0;
 
