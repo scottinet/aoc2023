d3ff8ad36f461d48a9a8415c13253a7ed6c3933d
Sébastien Cottinet <scottinet@protonmail.com>
2023-12-15 21:54:29 +0100

    refactor: less is more

--- day15/part1.ts
+++ day15/part1.ts
@@ -1,16 +1,11 @@
-function getNextValue(value: number, char: string): number {
-  value += char.charCodeAt(0);
-  return (value * 17) % 256;
-}
-
 export function part1(input: string): void {
   const parts = input.split(',');
   let sum = 0;
 
   for (const part of parts) {
     let value = 0;
-    for (const char of part) {
-      value = getNextValue(value, char);
+    for (let i = 0; i < part.length; i++) {
+      value = ((value + part.charCodeAt(i)) * 17) % 256;
     }
     sum += value;
   }
